ff683cc89e70de9f1aa633f14950ccdb4b450374
Swapnadeep Mukherjee <Swapnadeep.Mukherjee@ibm.com>
2024-11-17 21:23:20 +0530

    Pushing changes

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import controller.ToDoController;
 
 class Main {
     public static void main(String[] args) {
+        //controller module is called
         new ToDoController().runToDoApp();
+
     }
 }
